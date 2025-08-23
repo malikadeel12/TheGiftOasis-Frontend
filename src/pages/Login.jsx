aac7cb517492c922953a004415703c575ff2b306
Innocent boy <nomanriaz7980@gmail.com>
2025-08-23 06:23:43 -0700

    solve

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import jwtDecode from "jwt-decode";
-
+import { jwtDecode } from "jwt-decode";
 import api from "../services/api"; 
 
 const Login = () => {
