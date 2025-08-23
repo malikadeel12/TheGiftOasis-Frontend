1f51e821ca8e9540297c86e4568f3e4b97a84569
Innocent boy <nomanriaz7980@gmail.com>
2025-08-23 06:21:53 -0700

    add

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { jwtDecode } from "jwt-decode";
-import api from "../services/api"; // axios instance
+import jwtDecode from "jwt-decode";
+
+import api from "../services/api"; 
 
 const Login = () => {
   const navigate = useNavigate();
